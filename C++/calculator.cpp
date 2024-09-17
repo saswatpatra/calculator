d858075f8de28ae8e81eabce3504a91a033b5fc8
Saswat S. Patra <84391114+saswatpatra@users.noreply.github.com>
2024-09-17 20:13:31 +0530

    Update calculator.cpp

--- C++/calculator.cpp
+++ C++/calculator.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    char operation;
+    float num1, num2;
+
+    // Display options
+    cout << "Select operation (+, -, *, /): ";
+    cin >> operation;
+
+    // Get user input
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << "Result: " << num1 / num2 << endl;
+            else
+                cout << "Error! Division by zero." << endl;
+            break;
+        default:
+            cout << "Invalid operation!" << endl;
+            break;
+    }
+
+    return 0;
+}
